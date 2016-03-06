eeb281abaaaaa6a24a1494e71b054f9cc90f37f7
Lorente, Alejandro <eidansoft@gmail.com>
2016-03-06 19:32:26 +0100

    DDL Create table family

---
+++ backend/db/DML/family.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-03-2016 a las 19:10:25
+-- Versión del servidor: 5.5.46-0+deb8u1
+-- Versión de PHP: 5.6.14-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `bdecotex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `family`
+--
+
+CREATE TABLE IF NOT EXISTS `family` (
+`id_family` int(11) NOT NULL COMMENT 'Id',
+  `desc_family` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description'
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Family products';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `family`
+--
+ALTER TABLE `family`
+ ADD PRIMARY KEY (`id_family`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `family`
+--
+ALTER TABLE `family`
+MODIFY `id_family` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id',AUTO_INCREMENT=1;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
