b58b84e2d019e77870e7be5b2a55599d2bde477c
Lorente, Alejandro <eidansoft@gmail.com>
2016-06-19 19:02:12 +0200

    Añado la logica de creacion e importacion de entidades sexo

--- backend/app/tools/test.php
+++ backend/app/tools/test.php
@@ -1,8 +1,16 @@
 <?php
 require_once "./FamilyImporter.php.inc";
+require_once "./SexImporter.php.inc";
 
+echo "Procesando XML de Familias...";
 $familyImporter = new FamilyImporter();
 $familyImporter->truncateTable();
 $familyImporter->processImport();
+echo " XML procesado.\n\n";
 
-echo "XML procesado.";
+echo "Procesando XML de Sexo...";
+$sexImporter = new SexImporter();
+$sexImporter->truncateTable();
+$sexImporter->processImport();
+echo " XML procesado.\n\n";
+
