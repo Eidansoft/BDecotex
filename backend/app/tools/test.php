bf44631c452a09d551663bb01988058adf11ea8d
Lorente, Alejandro <eidansoft@gmail.com>
2016-08-11 00:11:03 +0200

    Añado test para los modelos

--- backend/app/tools/test.php
+++ backend/app/tools/test.php
@@ -2,6 +2,7 @@
 require_once "./FamilyImporter.php.inc";
 require_once "./SexImporter.php.inc";
 require_once "./LineImporter.php.inc";
+require_once "./ModelImporter.php.inc";
 
 echo "Procesando XML de Familias...";
 $familyImporter = new FamilyImporter();
@@ -9,6 +10,10 @@ $familyImporter->truncateTable();
 $familyImporter->processImport();
 echo " XML procesado.\n\n";
 
+echo "Procesando XML de Modelos...";
+$modelImporter = new ModelImporter();
+$modelImporter->truncateTable();
+$modelImporter->processImport();
 echo "Procesando XML de Sexo...";
 $sexImporter = new SexImporter();
 $sexImporter->truncateTable();
