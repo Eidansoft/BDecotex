4c242e5990e775e85d57d0af660e05437bc01877
Alejandro <alex@localhost.localdomain>
2016-09-03 17:29:56 +0200

    Extraido a una tlantilla externa la prueba del hola muindo

--- frontend/app/app.holaMundo.ts
+++ frontend/app/app.holaMundo.ts
@@ -1,16 +1,8 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import {Component} from 'angular2/core';
 
 @Component({ 
     selector: 'hola-mundo',
-    template: '<h1>Hola mundo en Angular2 !!!</h1>\n\
-                <p>Bienvenido <strong>{{nombre}}</strong>.\n\
-                Por favor, ten en cuenta que esto es una prueba y hay que tomarla como tal.</p>'
+    templateUrl: 'view/holaMundo.html'
     })
 
 export class HolaMundo {
