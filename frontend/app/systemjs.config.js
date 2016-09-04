8889be93ca6636a5eb55f1f7b4bc950836762fc7
Alejandro <alex@localhost.localdomain>
2016-09-04 14:36:40 +0200

    Extraida la configuracion del SystemConfig a un archivo externo.

---
+++ frontend/app/systemjs.config.js
@@ -0,0 +1,15 @@
+/**
+ * System configuration for Angular 2
+ */
+(function(global) {
+	var config = {
+		packages: {        
+			js: {
+				format: 'register',
+				defaultExtension: 'js'
+			}
+		}
+	};
+	
+	System.config(config);
+})(this);
