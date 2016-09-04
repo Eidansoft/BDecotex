b0ba035d279c80cb0770840559f5daaf9051aa89
Alejandro <alex@localhost.localdomain>
2016-09-04 21:38:20 +0200

    Añadido el componente de la ventana de familia

---
+++ frontend/app/controller/family.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({ 
+    selector: 'wecotex-family',
+    templateUrl: 'app/view/family.html'
+    })
+
+export class WecotexFamily {}
--- frontend/app/controller/window.ts
+++ frontend/app/controller/window.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {WecotexFamily} from './family'
 
 @Component({ 
     selector: 'wecotex-window',
-    templateUrl: 'app/view/window.html'
+    templateUrl: 'app/view/window.html',
+    directives: [WecotexFamily]
     })
 
 export class WecotexWindow {}
