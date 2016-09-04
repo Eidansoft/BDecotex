c8cef45d35d0e1a923369234edeea175b0495af8
Alejandro <alex@localhost.localdomain>
2016-09-04 12:24:23 +0200

    Finalizado hola mundo

--- frontend/app/app.holaMundo.ts
+++ frontend/app/app.holaMundo.ts
@@ -2,13 +2,20 @@ import {Component} from 'angular2/core';
 
 @Component({ 
     selector: 'hola-mundo',
-    templateUrl: 'view/holaMundo.html'
+    templateUrl: 'view/holaMundo.html',
+    styleUrls: ['../assets/css/holaMundo.css']
     })
 
 export class HolaMundo {
-    private nombre:string;
+    private nombre: string;
+    private mostrarTexto: boolean;
     
     public constructor(){
         this.nombre = "Alejandro Lorente";
+        this.mostrarTexto = false;
+    }
+    
+    public onClickBtn1(){
+        this.mostrarTexto = !this.mostrarTexto;
     }
 }
