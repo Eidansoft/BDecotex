bc83d144ae01a3ad4d714c4785de7a66e6a79967
Alejandro <alex@Alejandros-MacBook-Air.local>
2016-10-18 22:00:26 +0200

    añadidos los show and hide del icono de espera a cada llamada

--- frontend/app/controller/ControllerFamily.ts
+++ frontend/app/controller/ControllerFamily.ts
@@ -1,16 +1,20 @@
 class ControllerFamily
 {
-    static dependencies = ['ServiceBackend', ControllerFamily];
+    static dependencies = ['ServiceBackend', 'PubSub', ControllerFamily];
     private backendHandler: ServiceBackend;
+    private pubsubHandler: any;
     private familyList: Array<ModelFamily>;
     private familySelected: ModelFamily;
     
-    constructor(serviceBackend: ServiceBackend) {
+    constructor(serviceBackend: ServiceBackend, pubsub: any) {
         this.backendHandler = serviceBackend;
+        this.pubsubHandler = pubsub;
         
+        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
         this.backendHandler.getAllFamilies().then(
             successResponse => {
                 this.familyList = <Array<ModelFamily>>successResponse.data;
+                this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
             },
             errorResponse => {
                 console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
@@ -28,6 +32,7 @@ class ControllerFamily
     }
     
     protected saveFamily(){
+        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
         if (this.familySelected.id_family != undefined){
             this.backendHandler.updateFamily(this.familySelected).then(
                 successResponse => {
@@ -39,7 +44,8 @@ class ControllerFamily
                         return res;
                     });
                     this.familySelected = undefined;
-                    console.log("Familia salvada")
+                    console.log("Familia salvada");
+                    this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
                 },
                 errorResponse => {
                     console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
@@ -52,6 +58,7 @@ class ControllerFamily
                     this.familyList.push(<ModelFamily>successResponse.data);
                     console.log("Familia nueva creada");
                     this.familySelected = undefined;
+                    this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
                 },
                 errorResponse => {
                     console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
@@ -62,6 +69,7 @@ class ControllerFamily
     }
     
     protected deleteFamily(){
+        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
         this.backendHandler.deleteFamilyById(this.familySelected.id_family).then(
             successResponse => {
                 this.familyList = this.familyList.filter(family=>{
@@ -73,6 +81,7 @@ class ControllerFamily
                 });
                 console.log("Familia eliminada");
                 this.familySelected = undefined;
+                this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
             },
             errorResponse => {
                 console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
