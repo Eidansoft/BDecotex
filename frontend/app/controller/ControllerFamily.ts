34a6134d27e246cbcbc9ed61a9ab65fc01d76f8f
Alejandro <alex@Alejandros-MacBook-Air.local>
2016-10-18 22:34:45 +0200

    Refactorizada la gestion de las respuestas del servidor al servicio del backend, de manera que ahora quedan los controladores mas limpios y el codigo mas reutilizable

--- frontend/app/controller/ControllerFamily.ts
+++ frontend/app/controller/ControllerFamily.ts
@@ -1,25 +1,17 @@
 class ControllerFamily
 {
-    static dependencies = ['ServiceBackend', 'PubSub', ControllerFamily];
+    static dependencies = ['ServiceBackend', ControllerFamily];
     private backendHandler: ServiceBackend;
-    private pubsubHandler: any;
     private familyList: Array<ModelFamily>;
     private familySelected: ModelFamily;
     
-    constructor(serviceBackend: ServiceBackend, pubsub: any) {
+    constructor(serviceBackend: ServiceBackend) {
         this.backendHandler = serviceBackend;
-        this.pubsubHandler = pubsub;
         
-        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
-        this.backendHandler.getAllFamilies().then(
-            successResponse => {
-                this.familyList = <Array<ModelFamily>>successResponse.data;
-                this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
-            },
-            errorResponse => {
-                console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
-            }
-        );
+        this.backendHandler.getAllFamilies((familiesArray)=>{
+            this.familyList = <Array<ModelFamily>>familiesArray;
+            console.log("Familias recibidas");
+        });
     }
 
     protected selectFamily(family: ModelFamily){
@@ -32,62 +24,39 @@ class ControllerFamily
     }
     
     protected saveFamily(){
-        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
         if (this.familySelected.id_family != undefined){
-            this.backendHandler.updateFamily(this.familySelected).then(
-                successResponse => {
-                    this.familyList = this.familyList.map(family=>{
-                        let res = family;
-                        if (family.id_family == this.familySelected.id_family){
-                            res = this.familySelected;
-                        }
-                        return res;
-                    });
-                    this.familySelected = undefined;
-                    console.log("Familia salvada");
-                    this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
-                },
-                errorResponse => {
-                    console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
-                    this.familySelected = undefined;
-                }
-            );
+            this.backendHandler.updateFamily(this.familySelected, ()=>{
+                this.familyList = this.familyList.map(family=>{
+                    let res = family;
+                    if (family.id_family == this.familySelected.id_family){
+                        res = this.familySelected;
+                    }
+                    return res;
+                });
+                console.log("Familia editada");
+                this.familySelected = undefined;
+            });
         } else {
-            this.backendHandler.createNewFamily(this.familySelected).then(
-                successResponse => {
-                    this.familyList.push(<ModelFamily>successResponse.data);
-                    console.log("Familia nueva creada");
-                    this.familySelected = undefined;
-                    this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
-                },
-                errorResponse => {
-                    console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
-                    this.familySelected = undefined;
-                }
-            );
+            this.backendHandler.createNewFamily(this.familySelected, (createdFamily)=>{
+                this.familyList.push(<ModelFamily>createdFamily);
+                console.log("Familia nueva creada");
+                this.familySelected = undefined;
+            });
         }
     }
     
     protected deleteFamily(){
-        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
-        this.backendHandler.deleteFamilyById(this.familySelected.id_family).then(
-            successResponse => {
-                this.familyList = this.familyList.filter(family=>{
-                    let res = true;
-                    if (family.id_family == this.familySelected.id_family){
-                        res = false;
-                    }
-                    return res;
-                });
-                console.log("Familia eliminada");
-                this.familySelected = undefined;
-                this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
-            },
-            errorResponse => {
-                console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
-                this.familySelected = undefined;
-            }
-        );
+        this.backendHandler.deleteFamilyById(this.familySelected.id_family, ()=>{
+            this.familyList = this.familyList.filter(family=>{
+                let res = true;
+                if (family.id_family == this.familySelected.id_family){
+                    res = false;
+                }
+                return res;
+            });
+            console.log("Familia eliminada");
+            this.familySelected = undefined;
+        });
     }
 }
 
--- frontend/app/service/ServiceBackend.ts
+++ frontend/app/service/ServiceBackend.ts
@@ -1,19 +1,30 @@
 class ServiceBackend
 {
-    static dependencies = ['$http', ServiceBackend];
+    static dependencies = ['$http', 'PubSub', ServiceBackend];
     private httpHandler: ng.IHttpService;
+    private pubsubHandler: any;
     private backendPath: string;
     
-    constructor($http: ng.IHttpService){
+    constructor($http: ng.IHttpService, pubsub: any){
         this.httpHandler = $http;
+        this.pubsubHandler = pubsub;
         this.backendPath = '/bdecotex/backend/app';
     }
     
-    private getHttpHandler(requestConf: ng.IRequestConfig): ng.IPromise<any> {
-        return this.httpHandler(requestConf);
+    private getHttpHandler(requestConf: ng.IRequestConfig, onSuccessResult: Function): void {
+        this.pubsubHandler.publish('SHOW_WAITING_SCREEN', true);
+        this.httpHandler(requestConf).then(
+            (successResponse) => {
+                onSuccessResult(successResponse.data);
+                this.pubsubHandler.publish('HIDE_WAITING_SCREEN', true);
+            },
+            (errorResponse) => {
+                console.log("ERROR " + errorResponse.data.code + ": " + errorResponse.data.message);
+            }
+        );
     }
     
-    public createNewFamily(family: ModelFamily): ng.IHttpPromise<any>
+    public createNewFamily(family: ModelFamily, onSuccessResult: Function): void
     {
         var req = {
             method: 'POST',
@@ -21,36 +32,38 @@ class ServiceBackend
             data: family
         }
 
-        return this.getHttpHandler(req);
+        this.getHttpHandler(req, onSuccessResult);
     }
     
-    public getAllFamilies(): ng.IHttpPromise<any>
+    public getAllFamilies(onSuccessResult: Function): void
     {
         var req = {
             method: 'GET',
             url: this.backendPath + '/family'
         }
 
-        return this.getHttpHandler(req);
+        this.getHttpHandler(req, onSuccessResult);
     }
     
-    public updateFamily(family: ModelFamily): ng.IHttpPromise<any>{
+    public updateFamily(family: ModelFamily, onSuccessResult: Function): void
+    {
         var req = {
             method: 'PUT',
             url: this.backendPath + '/family/' + family.id_family,
             data: family
         }
 
-        return this.getHttpHandler(req);
+        this.getHttpHandler(req, onSuccessResult);
     }
     
-    public deleteFamilyById(id_family:number): ng.IHttpPromise<any>{
+    public deleteFamilyById(id_family:number, onSuccessResult: Function): void
+    {
         var req = {
             method: 'DELETE',
             url: this.backendPath + '/family/' + id_family
         }
 
-        return this.getHttpHandler(req);
+        this.getHttpHandler(req, onSuccessResult);
     }
 }
 
