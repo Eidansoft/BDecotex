c4e80a9d197d4b491b4316e2d646cd1d915cc55e
Alejandro Lorente <eidansoft@gmail.com>
2016-11-01 13:03:06 +0100

    Añadida vista y controlador para el modulo de sexo

--- frontend/app/controller/ControllerSex.ts
+++ frontend/app/controller/ControllerSex.ts
@@ -4,17 +4,39 @@ class ControllerSex
     private backendHandler: ServiceSexBackend;
     private sexList: Array<ModelSex>;
     private sexSelected: ModelSex;
+    private gridOptions: any;
+    private gridApi: uiGrid.IGridApi;
     
     constructor(serviceBakend: ServiceSexBackend)
     {
         this.backendHandler = serviceBakend;
         
+        this.gridOptions = {
+            columnDefs: [{ field: 'code', width: 100 }, { field: 'name' }],
+            enableRowSelection: true,
+            multiSelect: false,
+            enableRowHeaderSelection: false, //not checkbox
+            onRegisterApi: (api: uiGrid.IGridApi) => {
+                this.gridApi = api;
+            }
+        }
+        
         this.backendHandler.getAllSex((sexArray)=>{
             this.sexList = <Array<ModelSex>>sexArray;
+            this.gridOptions.data = this.sexList;
             console.log("Sexos recibidos");
         });
     }
 
+    protected editSex() {
+        let selections = this.gridApi.selection.getSelectedGridRows();
+        if (selections.length === 1){
+            this.selectSex(selections[0].entity);
+        } else {
+            alert("Por favor selecciona un sexo");
+        }
+    }
+    
     protected selectSex(sex: ModelSex){
         // Clone the object to edit to avoid modify the original
         this.sexSelected = (JSON.parse(JSON.stringify(sex)));
