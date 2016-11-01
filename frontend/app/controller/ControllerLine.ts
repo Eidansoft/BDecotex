503d0732fd7361432650fd22082eadb490aac249
Alejandro Lorente <eidansoft@gmail.com>
2016-11-01 01:11:08 +0100

    añadidos estilos y logica para linea

--- frontend/app/controller/ControllerLine.ts
+++ frontend/app/controller/ControllerLine.ts
@@ -4,15 +4,36 @@ class ControllerLine
     private backendHandler: ServiceLineBackend;
     private lineList: Array<ModelLine>;
     private lineSelected: ModelLine;
+    private gridOptions: any;
+    private gridApi: uiGrid.IGridApi;
     
-    constructor(serviceBakend: ServiceLineBackend)
-    {
+    constructor(serviceBakend: ServiceLineBackend) {
         this.backendHandler = serviceBakend;
+        
+        this.gridOptions = {
+            columnDefs: [{ field: 'code', width: 100 }, { field: 'name' }],
+            enableRowSelection: true,
+            multiSelect: false,
+            enableRowHeaderSelection: false, //not checkbox
+            onRegisterApi: (api: uiGrid.IGridApi) => {
+                this.gridApi = api;
+            }
+        }
         
         this.backendHandler.getAllLines((linesArray)=>{
             this.lineList = <Array<ModelLine>>linesArray;
+            this.gridOptions.data = this.lineList;
             console.log("Lineas recibidas");
         });
+    }
+
+    protected editFamily() {
+        let selections = this.gridApi.selection.getSelectedGridRows();
+        if (selections.length === 1){
+            this.selectLine(selections[0].entity);
+        } else {
+            alert("Por favor selecciona una linea");
+        }
     }
 
     protected selectLine(line: ModelLine){
@@ -55,6 +76,7 @@ class ControllerLine
                 }
                 return res;
             });
+            this.gridOptions.data = this.lineList;
             console.log("Linea eliminada");
             this.lineSelected = undefined;
         });
