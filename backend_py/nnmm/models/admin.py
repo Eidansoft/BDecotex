7130c9bd4f17c974b32fa8a6f5bcbb1700c4d58e
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-05 21:59:31 +0100

    Actualizada vista de modelos para poder filtrar buscar y que el variante se genere automaticamente

--- backend_py/nnmm/models/admin.py
+++ backend_py/nnmm/models/admin.py
@@ -2,8 +2,37 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Sex, Model
+from .models import Sex, Line, Family, Model
 
 # Register your models here.
 admin.site.register(Sex)
-admin.site.register(Model)
+admin.site.register(Line)
+admin.site.register(Family)
+
+class ModelAdministration(admin.ModelAdmin):
+    fieldsets = [
+        ('Type', {'fields': ['family', 'line', 'sex']}),
+        ('Data', {'fields': ['description', 'notes']}),
+        ('Properties', {'fields': ['front', 'back', 'neck', 'arm', 'client'], 'classes': ['collapse']}),
+        ('Relations', {'fields': ['parent', 'old_code_parent'], 'classes': ['collapse']}),
+    ]
+    
+    list_display = ('model_code', 'description')
+    list_filter = ['sex', 'family', 'line']
+    search_fields = ['description', 'notes']
+
+    def save_model(self, request, obj, form, change):
+        import ipdb; ipdb.set_trace()
+        # To add a new model, we need to calculate the new variant
+        if not change and form.is_valid():
+            family = form.cleaned_data['family']
+            line = form.cleaned_data['line']
+            sex = form.cleaned_data['sex']
+            
+            obj.variant = Model.generate_next_free_variant(
+                family, line, sex
+            )
+
+        super(ModelAdministration, self).save_model(request, obj, form, change)
+
+admin.site.register(Model, ModelAdministration)
--- backend_py/nnmm/models/models.py
+++ backend_py/nnmm/models/models.py
@@ -12,10 +12,61 @@ class Sex(models.Model):
         return self.name
 
 
+class Line(models.Model):
+    code = models.CharField(max_length=2, unique=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Family(models.Model):
+    code = models.CharField(max_length=2, unique=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Model(models.Model):
-    sex = models.ForeignKey(Sex, on_delete=models.CASCADE)
-    description = models.CharField(max_length=200)
+    sex = models.ForeignKey(Sex, on_delete=models.PROTECT)
+    line = models.ForeignKey(Line, on_delete=models.PROTECT)
+    family = models.ForeignKey(Family, on_delete=models.PROTECT)
+    variant = models.PositiveSmallIntegerField()
+
+    description = models.CharField(max_length=100)
+    front = models.CharField(max_length=100, null= True, blank=True, default='')
+    back = models.CharField(max_length=100, null= True, blank=True, default='')
+    neck = models.CharField(max_length=100, null= True, blank=True, default='')
+    arm = models.CharField(max_length=100, null= True, blank=True, default='')
+    notes = models.TextField(max_length=3000, null= True, blank=True, default='')
+    client = models.CharField(max_length=100, null= True, blank=True, default='')
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null= True, blank=True, default='')
+    old_code_parent = models.CharField(max_length=10, null= True, blank=True, default='')
+
+    def model_code(self):
+        return "{}{}{}{}".format(self.family.code, self.line.code, self.sex.code, self.variant)
 
     def __str__(self):
         return self.description
 
+    @staticmethod
+    def generate_next_free_variant(family, line, sex):
+        '''
+        This method calculate the next free variant for a given
+        family, line and sex
+        '''
+        res = 0
+
+        models = Model.objects.filter(family=family, line=line, sex=sex)
+        if models:
+            already_existent_variants = [
+                model.variant for model in models
+            ]
+            res = [
+                n
+                for n in xrange(len(already_existent_variants)+1)
+                if n not in already_existent_variants
+            ][0]
+        
+        return res
