aaeb919e14f5389b044cdf2289a10f952625cb84
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-18 18:44:50 +0100

    Añado url para la aplicacion de importacion

--- backend_py/nnmm/nnmm/urls.py
+++ backend_py/nnmm/nnmm/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^models/', include('models.urls')),
+    url(r'^admin/import/', include('importer.urls')),
     url(r'^admin/', admin.site.urls),
 ]
