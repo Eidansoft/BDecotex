d708b162b29ad25d0dfc80df0fe219a31fcf4af9
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-18 18:46:29 +0100

    añado la aplicacion de importacion a las aplicacione sinstaladas

--- backend_py/nnmm/nnmm/settings.py
+++ backend_py/nnmm/nnmm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'models.apps.ModelsConfig',
+    'importer.apps.ImporterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
