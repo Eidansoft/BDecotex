20f0046b5c95cf9a838fdbde88506491e4307aca
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-19 18:53:34 +0100

    Remove add button at django admin

--- backend_py/nnmm/importer/admin.py
+++ backend_py/nnmm/importer/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Mdb
 
 # Register your models here.
+
+class ImporterAdministration(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(Mdb, ImporterAdministration)
