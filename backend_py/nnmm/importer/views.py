8023ef96c799a9ae0ecb508fa5ac4d463a7e7913
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-19 18:55:16 +0100

    Actiualizo vista para iniciar el uso de la libreria mdb

--- backend_py/nnmm/importer/views.py
+++ backend_py/nnmm/importer/views.py
@@ -5,19 +5,51 @@ from django.shortcuts import render
 
 # Create your views here.
 from forms import ImportForm
+from MdbAccessReader import MdbAccessReader
+from nnmm.models.models import Family
 
 
 def mdbimport(request):
 
+    context = {}
     if request.method == 'POST':
         form = ImportForm(request.POST, request.FILES)
         if form.is_valid():
-            pass
-            #handle_uploaded_file(request.FILES['file'])
+            import ipdb; ipdb.set_trace(context=21)
+            handle_uploaded_file(request.FILES['file'])
             return render(request, 'importer/index.html')
-    else:
-        form = ImportForm()
+        else:
+            context['error'] = 'Datos no validos'
 
-    context = {'form': form}
+    form = ImportForm()
+    context['form'] = form
     return render(request, 'importer/index.html', context)
+
+def handle_uploaded_file(f):
+    temp_file = 'dbimport.mdb'
+    with open(temp_file, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    import ipdb; ipdb.set_trace(context=21)
+
+    families = MdbAccessReader.get_content_table_dict(temp_file, 'Familias')
+    save_families(families)
+    
+    # lines = MdbAccessReader.get_content_table_dict(temp_file, 'Lineas')
+    # save_lines(lines)
+    
+    # sexs = MdbAccessReader.get_content_table_dict(temp_file, 'Sexo')
+    # save_sex(sexs)
+    
+    # models = MdbAccessReader.get_content_table_dict(temp_file, 'Nuevos_Modelos')
+    # save_models(models)
+
+def save_families(families):
+    for fam in families:
+        f= Family.objects.create(code=fam.get('Cod'),
+                              name=fam.get('Desc'))
+        f.save()
+
+
+
 
