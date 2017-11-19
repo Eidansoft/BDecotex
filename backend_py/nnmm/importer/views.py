a22e7bf93c97e9e9fd13d1b120546e054bf7f851
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-11-19 22:02:58 +0100

    Proceso los datos del mdb y los importo

--- backend_py/nnmm/importer/views.py
+++ backend_py/nnmm/importer/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 from forms import ImportForm
 from MdbAccessReader import MdbAccessReader
-# from nnmm.models.models import Family
+from modelo.models import Family, Line, Sex, Modelo
 
 
 def mdbimport(request):
@@ -15,7 +15,6 @@ def mdbimport(request):
     if request.method == 'POST':
         form = ImportForm(request.POST, request.FILES)
         if form.is_valid():
-            import ipdb; ipdb.set_trace(context=21)
             handle_uploaded_file(request.FILES['file'])
             return render(request, 'importer/index.html')
         else:
@@ -30,26 +29,44 @@ def handle_uploaded_file(f):
     with open(temp_file, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
-    import ipdb; ipdb.set_trace(context=21)
 
     families = MdbAccessReader.get_content_table_dict(temp_file, 'Familias')
     save_families(families)
     
-    # lines = MdbAccessReader.get_content_table_dict(temp_file, 'Lineas')
-    # save_lines(lines)
+    lines = MdbAccessReader.get_content_table_dict(temp_file, 'Lineas')
+    save_lines(lines)
     
-    # sexs = MdbAccessReader.get_content_table_dict(temp_file, 'Sexo')
-    # save_sex(sexs)
+    sexs = MdbAccessReader.get_content_table_dict(temp_file, 'Sexo')
+    save_sex(sexs)
     
-    # models = MdbAccessReader.get_content_table_dict(temp_file, 'Nuevos_Modelos')
-    # save_models(models)
+    models = MdbAccessReader.get_content_table_dict(temp_file, 'Nuevos_Modelos')
+    save_models(models)
 
 def save_families(families):
     for fam in families:
-        # f= Family.objects.create(code=fam.get('Cod'),
-                              # name=fam.get('Desc'))
-        # f.save()
-        pass
+        f= Family.objects.create(code=fam.get('Cod'),
+                              name=fam.get('Desc'))
+        f.save()
+
+def save_lines(lines):
+    for lin in lines:
+        l= Line.objects.create(code=lin.get(u'Línea'),
+                              name=lin.get(u'Descripción'))
+        l.save()
+
+def save_sex(sexs):
+    for sex in sexs:
+        s = Sex.objects.create(code=sex.get(u'Código'),
+                              name=sex.get(u'Sexo'))
+        s.save()
+
+
+def save_models(lista_modelos):
+    import ipdb; ipdb.set_trace(context=21)
+    for modelo in lista_modelos:
+        m = Modelo.objects.create(code=modelo.get(u'Línea'),
+                              name=modelo.get(u'Descripción'))
+        m.save()
 
 
 
