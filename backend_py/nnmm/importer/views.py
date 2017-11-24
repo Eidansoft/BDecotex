f61d6f63399cd48462355e1dd111b19df15b78d8
Eidansoft <eidansoft@gmail.com>
2017-11-24 22:13:25 +0100

    Arregalr dependencias que fallaban en win

--- backend_py/nnmm/importer/views.py
+++ backend_py/nnmm/importer/views.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from forms import ImportForm
-from MdbAccessReader import MdbAccessReader
+from .forms import ImportForm
+from .MdbAccessReader import MdbAccessReader
 from modelo.models import Family, Line, Sex, Modelo
 
 
